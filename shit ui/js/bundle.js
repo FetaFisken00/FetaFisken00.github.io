726606ab082bf3b451cd3fe8704e121ec2ecebc5
FetaFisken00 <Cullberg.aron@gmail.com>
2022-01-30 17:40:13 +0100

    shit ui is kinda working

--- shit ui/js/bundle.js
+++ shit ui/js/bundle.js
@@ -1,24 +1,23 @@
-dragElement(<HTMLElement>document.getElementById('window'));
-
-function dragElement(elemt: HTMLElement) {
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+"use strict";
+dragElement(document.getElementById('window'));
+function dragElement(elemt) {
     let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     if (document.getElementById(elemt.id + "Header")) {
-        /* 
+        /*
         if present, the header is where you move the DIV from:
         */
-        (<HTMLElement>document.getElementById(elemt.id + "Header")).addEventListener('mousedown', dragMouseDown);
+        document.getElementById(elemt.id + "Header").addEventListener('mousedown', dragMouseDown);
     }
-
-    function dragMouseDown(e: MouseEvent) {
-        e.preventDefault()
+    function dragMouseDown(e) {
+        e.preventDefault();
         pos3 = e.clientX;
         pos4 = e.clientY;
         document.addEventListener('mouseup', closeDragElement);
         document.addEventListener('mousemove', elementDrag);
     }
-
-    function elementDrag(e: MouseEvent) {
-        e.preventDefault()
+    function elementDrag(e) {
+        e.preventDefault();
         pos1 = pos3 - e.clientX;
         pos2 = pos4 - e.clientY;
         pos3 = e.clientX;
@@ -30,9 +29,11 @@ function dragElement(elemt: HTMLElement) {
             elemt.style.left = (elemt.offsetLeft - pos1) + "px";
         }
     }
-
     function closeDragElement() {
         document.removeEventListener('mouseup', closeDragElement);
         document.removeEventListener('mousemove', elementDrag);
     }
-};
+}
+;
+
+},{}]},{},[1]);
--- shit ui/js/main.js
+++ shit ui/js/main.js
@@ -1,24 +1,22 @@
-dragElement(<HTMLElement>document.getElementById('window'));
-
-function dragElement(elemt: HTMLElement) {
+"use strict";
+dragElement(document.getElementById('window'));
+function dragElement(elemt) {
     let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     if (document.getElementById(elemt.id + "Header")) {
-        /* 
+        /*
         if present, the header is where you move the DIV from:
         */
-        (<HTMLElement>document.getElementById(elemt.id + "Header")).addEventListener('mousedown', dragMouseDown);
+        document.getElementById(elemt.id + "Header").addEventListener('mousedown', dragMouseDown);
     }
-
-    function dragMouseDown(e: MouseEvent) {
-        e.preventDefault()
+    function dragMouseDown(e) {
+        e.preventDefault();
         pos3 = e.clientX;
         pos4 = e.clientY;
         document.addEventListener('mouseup', closeDragElement);
         document.addEventListener('mousemove', elementDrag);
     }
-
-    function elementDrag(e: MouseEvent) {
-        e.preventDefault()
+    function elementDrag(e) {
+        e.preventDefault();
         pos1 = pos3 - e.clientX;
         pos2 = pos4 - e.clientY;
         pos3 = e.clientX;
@@ -30,9 +28,10 @@ function dragElement(elemt: HTMLElement) {
             elemt.style.left = (elemt.offsetLeft - pos1) + "px";
         }
     }
-
     function closeDragElement() {
         document.removeEventListener('mouseup', closeDragElement);
         document.removeEventListener('mousemove', elementDrag);
     }
-};
+}
+;
+//# sourceMappingURL=main.js.map
