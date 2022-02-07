bb2f722fca7e1457a74cc0ca252ea3278bed63bd
FetaFisken00 <Cullberg.aron@gmail.com>
2022-02-07 11:32:23 +0100

    gulp working

--- shit ui/gulpfile.js
+++ shit ui/gulpfile.js
@@ -1,32 +1,35 @@
-import pkg from "gulp";
-const { task, src, dest, series, parallel } = pkg;
+import gulp from 'gulp'
+const { task, src, dest, series, parallel } = gulp;
 import browserify from "browserify";
 import source from "vinyl-source-stream";
-import watchify from "watchify";
 import tsify from "tsify";
-import fancy_log from "fancy-log";
+import uglify from "gulp-uglify";
+import gulpSourcemaps from 'gulp-sourcemaps'
+const { init, write } = gulpSourcemaps;
+import buffer from "vinyl-buffer";
 var paths = {
   pages: ["src/*.html"],
 };
-var watchedBrowserify = watchify(
-  browserify({
-    basedir: ".",
-    debug: true,
-    entries: ["src/main.ts"],
-    cache: {},
-    packageCache: {},
-  }).plugin(tsify)
-);
 task("copy-html", function () {
   return src(paths.pages).pipe(dest("dist"));
 });
-function bundle() {
-  return watchedBrowserify
-    .bundle()
-    .on("error", fancy_log)
-    .pipe(source("bundle.js"))
-    .pipe(dest("dist"));
-}
-task("default", series(parallel("copy-html"), bundle));
-watchedBrowserify.on("update", bundle);
-watchedBrowserify.on("log", fancy_log);
+task(
+  "default",
+  series(parallel("copy-html"), function () {
+    return browserify({
+      basedir: ".",
+      debug: true,
+      entries: ["src/main.ts"],
+      cache: {},
+      packageCache: {},
+    })
+      .plugin(tsify)
+      .bundle()
+      .pipe(source("bundle.js"))
+      .pipe(buffer())
+      .pipe(init({ loadMaps: true }))
+      .pipe(uglify())
+      .pipe(write("./"))
+      .pipe(dest("dist"));
+  })
+);
